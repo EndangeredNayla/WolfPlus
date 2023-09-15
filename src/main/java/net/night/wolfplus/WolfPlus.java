eda16a352e7c581562095599b9af9e6e21c205d8
Aleft2 <118831347+Aleft2@users.noreply.github.com>
2023-09-14 21:29:21 -0300

    Add files via upload

---
+++ src/main/java/net/night/wolfplus/WolfPlus.java
@@ -0,0 +1,20 @@
+package net.night.wolfplus;
+
+import net.minecraft.block.Block;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = WolfPlus.MODID, version = WolfPlus.VERSION)
+public class WolfPlus
+{
+    public static final String MODID = "WolfPlus";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+	  EntityRegistry.registerGlobalEntityID(EntityWolf00.class, "Wolf00", EntityRegistry.findGlobalUniqueEntityId(), 14144467, 3858176);
+      EntityRegistry.addSpawn(EntityWolf00.class, 50, 2, 6);
+    }
+}
